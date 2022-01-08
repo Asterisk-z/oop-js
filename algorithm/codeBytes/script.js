e1231ddbd1190df27807f8d3031ae37fa91952ed
Olang Daniel <56372709+Asterisk-z@users.noreply.github.com>
2022-01-08 23:59:56 +0100

    catalan

--- algorithm/codeBytes/script.js
+++ algorithm/codeBytes/script.js
@@ -5,3 +5,19 @@ function permutation(number) {
 // console.log(permutation(10));
 // console.log(permutation(4));
 // console.log(permutation(8));
+
+function catalan(number) {
+
+    // let factorial = (num) => {
+    //     return (num === 1 ) ? 1 : num * factorial(num - 1);
+    // }
+
+    // return (factorial(2 * number)) / ((factorial(number + 1)) * (factorial(number)));
+
+    return (permutation(2 * number)) / ((permutation(number + 1)) * (permutation(number)));
+
+}
+
+console.log(catalan(3));
+console.log(catalan(5));
+console.log(catalan(10));
